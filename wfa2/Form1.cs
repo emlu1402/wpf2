5f61627fd232a32cdbfb9a649970778626724dc7
Emilia Lundgren <emlu1402@student.miun.se>
2017-09-05 09:40:58 +0200

    Lagt till en knapp som visar en hälsning

--- wfa2/Form1.cs
+++ wfa2/Form1.cs
@@ -16,5 +16,10 @@ namespace wfa2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hej");
+        }
     }
 }
